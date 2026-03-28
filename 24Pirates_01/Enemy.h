003d2612a4d7e0ad1ecb21815ccbc820905ec2d5
LAPTOP-T5S1L1O2\vnfms <vnfmstldnjd@gmail.com>
2026-03-29 04:28:36 +0900

    몬스터 기본 cpp, h파일 작업

---
+++ 24Pirates_01/Enemy.h
@@ -0,0 +1,49 @@
+﻿// Enemy.h
+
+#pragma once
+#include <string>
+#include <vector>
+#include <iostream>
+
+// 스킬 타입 구분 (일반 / 고유)
+enum class SkillType
+{
+    Normal,
+    Unique
+};
+
+// 몬스터 스킬 구조체
+struct EnemySkill
+{
+    std::string skillName;
+    SkillType type;
+    int skillValue;
+};
+
+class Enemy
+{
+protected:
+    std::string engName;
+    int maxHp;
+    int currentHp;
+    int atk;
+    int def;
+    int getExp;
+    int getGold;
+
+    // 스킬 목록
+    std::vector<EnemySkill> skills;
+
+    int turnCount = 0;
+
+public:
+    Enemy(std::string name, int baseHp, int baseAtk, int baseDef, int baseExp, int baseGold, int roomCount);
+    virtual ~Enemy() = default;
+
+    virtual void ApplyScaling(int roomCount);
+    virtual void TakeDamage(int damage);
+    bool IsDead() const;
+
+    virtual EnemySkill DetermineNextSkill() = 0;
+    void EndTurn() { turnCount++; }
+};
