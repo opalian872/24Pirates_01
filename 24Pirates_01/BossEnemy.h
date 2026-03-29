9f87e7b6597f841ede983e5fc0908f2045509963
LAPTOP-T5S1L1O2\vnfms <vnfmstldnjd@gmail.com>
2026-03-30 00:36:11 +0900

    일반 몬스터, 보스 몬스터 DB 추가 추가 한 db는 밸런스 패치로 추후 수정 가능

---
+++ 24Pirates_01/BossEnemy.h
@@ -0,0 +1,22 @@
+﻿// BossEnemy
+
+#pragma once
+#include "Enemy.h"
+
+enum class BossType
+{
+    SkeletonKing,
+    OrcKing
+};
+
+class BossEnemy : public Enemy
+{
+public:
+    BossEnemy(BossType type, int roomCount);
+    virtual EnemySkill DetermineNextSkill() override;
+
+private:
+    BossType bossType;
+    // 페이즈 변환 있을 시 활성화
+    // int phase =1;
+};
---
+++ 24Pirates_01/NormalEnemy.h
@@ -0,0 +1,26 @@
+﻿// NormalEnemy.h
+
+#pragma once
+#include "Enemy.h"
+
+enum class NormalType
+{
+    Skeleton,
+    Goblin,
+    Snail,
+    Orc,
+    Hog,
+    Zombie,
+    Golem,
+    Slime,
+    Chimera
+};
+
+class NormalEnemy : public Enemy
+{
+public:
+    // roomCount에 따른 레벨 스케일링
+    NormalEnemy(NormalType type, int roomCount);
+
+    virtual EnemySkill DetermineNextSkill() override;
+};
