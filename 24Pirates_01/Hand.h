1d0507ecf7fadda4c41ce3dd13ac3933cc90404f
MIN <nakbkqwe21@gmail.com>
2026-03-30 13:15:36 +0900

    passive 카드 구현 passive 카드 해제 구현 시작 덱 Normal 카드 고정 빌드 가능 버전

--- 24Pirates_01/Hand.h
+++ 24Pirates_01/Hand.h
@@ -1,10 +1,9 @@
-﻿// Hand.h
-
-#pragma once
+﻿#pragma once
 
 #include <vector>
 #include "Card.h"
 #include "Deck.h"
+#include "Player.h"
 
 class Hand
 {
@@ -15,15 +14,21 @@ public:
     Hand();
     ~Hand();
 
-    void DrawCards(const Deck& deck, int count);
+    // Draw cards at the start of a turn
+    void DrawCards(const Deck& deck, int count, Player& player);
 
-    void addCard(Card* card);
+    // Add one card during the turn (for draw effects)
+    void addCard(Card* card, Player& player);
 
+    // Get one card
     Card* getCard(int index) const;
 
+    // Number of cards in hand
     int getCardCount() const;
 
+    // Return all cards for UI
     const std::vector<Card*>& getCards() const;
 
-    void Clear();
+    // Clear hand at end of turn
+    void Clear(Player& player);
 };
