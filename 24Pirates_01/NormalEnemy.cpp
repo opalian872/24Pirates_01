c306347ba62906198f7af763216062e2ab3b9b8c
LAPTOP-T5S1L1O2\vnfms <vnfmstldnjd@gmail.com>
2026-03-31 14:30:34 +0900

    몬스터의 경험치 1.5배 증가

--- 24Pirates_01/NormalEnemy.cpp
+++ 24Pirates_01/NormalEnemy.cpp
@@ -20,7 +20,7 @@ NormalEnemy::NormalEnemy(NormalType type, int roomCount)
 
     case NormalType::Goblin:
         engName = "Semicolon Thief Goblin";
-        maxHp = 40; atk = 16; def = 6; getExp = 20; getGold = 15;
+        maxHp = 40; atk = 16; def = 6; getExp = 30; getGold = 15;
 
         skills.push_back({ "Goblin Stab Attack", SkillType::Normal, atk });
         skills.push_back({ "ATTACKKKK!!!!!", SkillType::Normal, static_cast<int>(atk * 0.5f) });
@@ -29,7 +29,7 @@ NormalEnemy::NormalEnemy(NormalType type, int roomCount)
 
     case NormalType::Snail:
         engName = "The Infinite Loop Snail";
-        maxHp = 30; atk = 4; def = 16; getExp = 20; getGold = 15;
+        maxHp = 30; atk = 4; def = 16; getExp = 30; getGold = 15;
 
         skills.push_back({ "Special Snail Cream Attack", SkillType::Normal, atk});
         skills.push_back({ "SUPER FAST MOVING", SkillType::Normal, 0 });
@@ -38,7 +38,7 @@ NormalEnemy::NormalEnemy(NormalType type, int roomCount)
 
     case NormalType::Orc:
         engName = "The Null Pointer Orc";
-        maxHp = 50; atk = 16; def = 10; getExp = 20; getGold = 20;
+        maxHp = 50; atk = 16; def = 10; getExp = 30; getGold = 20;
 
         skills.push_back({ "Orc Thumping Attack", SkillType::Normal, atk });
         skills.push_back({ "Orc Smash", SkillType::Normal, static_cast<int>(atk * 1.5f) });
@@ -47,7 +47,7 @@ NormalEnemy::NormalEnemy(NormalType type, int roomCount)
 
     case NormalType::Hog:
         engName = "Permission Denied Hog";
-        maxHp = 100; atk = 20; def = 20; getExp = 20; getGold = 15;
+        maxHp = 100; atk = 20; def = 20; getExp = 30; getGold = 15;
 
         skills.push_back({ "Rush", SkillType::Normal, atk });
         skills.push_back({ "RU-----------------SH", SkillType::Normal, static_cast<int>(atk * 0.5f) });
@@ -56,7 +56,7 @@ NormalEnemy::NormalEnemy(NormalType type, int roomCount)
 
     case NormalType::Zombie:
         engName = "Missing Virtual Zombie";
-        maxHp = 100; atk = 30; def = 10; getExp = 20; getGold = 15;
+        maxHp = 100; atk = 30; def = 10; getExp = 30; getGold = 15;
 
         skills.push_back({ "Zombie Claw Attack", SkillType::Normal, atk });
         skills.push_back({ "Fatching Somthing", SkillType::Normal, static_cast<int>(atk * 0.5f) });
@@ -65,7 +65,7 @@ NormalEnemy::NormalEnemy(NormalType type, int roomCount)
 
     case NormalType::Golem:
         engName = "Inline Failed Golem";
-        maxHp = 150; atk = 20; def = 30; getExp = 20; getGold = 20;
+        maxHp = 150; atk = 20; def = 30; getExp = 30; getGold = 20;
 
         skills.push_back({ "Golem Attack", SkillType::Normal, atk });
         skills.push_back({ "Golem Hyper Attack", SkillType::Normal, atk });
@@ -74,7 +74,7 @@ NormalEnemy::NormalEnemy(NormalType type, int roomCount)
 
     case NormalType::Slime:
         engName = "One-Liner Slime";
-        maxHp = 120; atk = 20; def = 25; getExp = 20; getGold = 25;
+        maxHp = 120; atk = 20; def = 25; getExp = 30; getGold = 25;
 
         skills.push_back({ "Slime Smile Attack", SkillType::Normal, atk });
         skills.push_back({ "Self Healing", SkillType::Normal, static_cast<int>(maxHp * 0.1f) });
@@ -83,7 +83,7 @@ NormalEnemy::NormalEnemy(NormalType type, int roomCount)
 
     case NormalType::Chimera:
         engName = "Zero-Indent Chimera";
-        maxHp = 250; atk = 25; def =35; getExp = 20; getGold =40;
+        maxHp = 250; atk = 25; def =35; getExp = 30; getGold =40;
 
         skills.push_back({ "Somthing Terrible Attack", SkillType::Normal, atk });
         skills.push_back({ "Maybe Awesome Attack", SkillType::Normal, static_cast<int>(atk * 0.5) });
